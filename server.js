cde1e29a9787753394c423f033afcff58379a5ce
Hallenbeck <RCHallen92@gmail.com>
2020-04-14 15:10:03 -0600

    changed express notation in server.js removed JSON reference removed app.js

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ var PORT= process.env.PORT || 5000;
 // Ejs
 // ================================================================================================
 
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-// require("./routes/api-routes.js")(app);
-
-app.use(express.json());
-
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static('./public'));
 app.use('/jquery', express.static('node_modules/jquery/dist'));
 app.use('/whatInput', express.static('node_modules/what-input/dist'));
 app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 app.use('/', require(__dirname + '/routes/html-routes'));
+// require("./routes/api-routes.js")(app);
+
+app.use(express.json());
+
+
+
 
 app.listen(PORT, console.log("server start on " + PORT));
