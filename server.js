3cff182a9a506f51ba16e868afb1da730bb0c7e6
Hallenbeck <RCHallen92@gmail.com>
2020-04-14 03:18:46 -0600

    changed express notation in server.js removed JSON reference removed app.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const app = express();
 const expressLayouts = require('express-ejs-layouts');
-
+const app = express();
 const PORT= process.env.PORT || 5000;
 
 // Ejs
 // ================================================================================================
 
-app.use(expressLayouts);
+
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 // require("./routes/api-routes.js")(app);
 
 // app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
 // app.use('/public', express.static('./public'));
-// app.use('/jquery', express.static('node_modules/jquery/dist'));
-// app.use('/whatInput', express.static('node_modules/what-input/dist'));
-// app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/whatInput', express.static('node_modules/what-input/dist'));
+app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
 // app.use('/json', express.static('public/thumbnails.json'));
 
 app.use('/', require(__dirname + '/routes/html-routes'));
