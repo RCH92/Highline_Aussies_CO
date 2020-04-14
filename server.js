d837dd5c092408f2c925ea7cad6f93ceaf7ddbac
Hallenbeck <RCHallen92@gmail.com>
2020-04-14 14:49:20 -0600

    changed express notation in server.js removed JSON reference removed app.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
-// app.use('/public', express.static('./public'));
+app.use('/public', express.static('./public'));
 app.use('/jquery', express.static('node_modules/jquery/dist'));
 app.use('/whatInput', express.static('node_modules/what-input/dist'));
 app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
