bf7582942182d0032828191e171b5fb66bcd5a6b
Hallenbeck <RCHallen92@gmail.com>
2020-04-15 03:59:58 -0600

    moved css

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+process.env.PWD = process.cwd();
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
@@ -6,24 +7,24 @@ var PORT= process.env.PORT || 5000;
 
 // Ejs
 // ================================================================================================
-app.use('/css', express.static(path.join(__dirname, '/public/css')));
-
+// app.use('/css', express.st??atic(path.join(__dirname, '/public/css')));
+app.use('/css', express.static(path.join(process.env.PWD, 'public/css')));
 app.use(expressLayouts);
 app.use(express.json());
 app.set('view engine', 'ejs');
 
 
-app.use('/images', express.static(__dirname + '/public/images'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/files', express.static(__dirname + '/public/files'));
+// app.use('/images', express.static(__dirname + '/public/images'));
+// app.use('/js', express.static(__dirname + '/public/js'));
+// app.use('/files', express.static(__dirname + '/public/files'));
 
 
 
 // app.use(express.static(__dirname + '/public'));
 
-app.use('/jquery', express.static('node_modules/jquery/dist'));
-app.use('/whatInput', express.static('node_modules/what-input/dist'));
-app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
+// app.use('/jquery', express.static('node_modules/jquery/dist'));
+// app.use('/whatInput', express.static('node_modules/what-input/dist'));
+// app.use('/foundation', express.static('node_modules/foundation-sites/dist'));
 app.use('/', require(__dirname + '/routes/html-routes'));
 
 // app.use('/public', express.static('./public'));
